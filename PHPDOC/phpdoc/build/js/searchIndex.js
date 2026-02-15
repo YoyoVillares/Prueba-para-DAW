765e8bb197e188d7b0472e87b39259919ffd8b19
YoyoVillares <yoelvillarescastro@gmail.com>
2026-02-15 02:50:35 +0100

    Add files via upload
    Segundo commit con los archivos generados por doc ya que al tener un punto delante la carpeta y ser oculta no me dejaba subirlos al principio

---
+++ PHPDOC/phpdoc/build/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\sumar\u0028\u0029",
+            "name": "sumar",
+            "summary": "Suma\u0020dous\u0020valores.",
+            "url": "namespaces/default.html#function_sumar"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
